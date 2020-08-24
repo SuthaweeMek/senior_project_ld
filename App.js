f992514eac3d8a128326eee6720ab6932ba89d19
Ongsa <ongsa.sungkanit@gmail.com>
2020-08-24 16:33:23 +0700

    set file structure

--- App.js
+++ App.js
@@ -41,8 +41,8 @@ return (
     <View style={{ flex: 1, flexDirection: 'row' }}>
       <SketchCanvas
         style={{ flex: 1 }}
-        strokeColor={'red'}
-        strokeWidth={7}
+        strokeColor={'black'}
+        strokeWidth={3}
         ref={canvasRef}
       />
     </View>
