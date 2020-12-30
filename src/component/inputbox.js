a1c9e0eb1d5ccbecab5b1add7c5575dc8ef9b8d5
SuthaweeMek <suthawee.w@gmail.com>
2020-12-30 15:59:58 +0700

    Login Api

--- src/component/inputbox.js
+++ src/component/inputbox.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { TextInput } from 'react-native';
 
-const inputbox = ({text,onChangeText}) => {
+const inputbox = (props) => {
   return (
     <TextInput
       style={{ height: 40,width:160,marginTop:5,marginBottom:5,borderColor: 'gray', borderWidth: 1 }}
-      onChangeText={text => onChangeText(text)}
-      value={text}
+      onChangeText={text => props.onChangeText(text)}
+      value={props.text}
+      placeholder = {props.placeholder}
     />
   );
 }
--- src/scene/LoginScene.js
+++ src/scene/LoginScene.js
@@ -6,7 +6,7 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, { useState } from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -25,43 +25,101 @@ import backgroundLogin from '../resource/image/backgroundLogin.png'
 import Router from '../router'
 import ButtonCurve from '../component/buttonCurve';
 import InputBox from '../component/inputbox';
+import AsyncStorage from '@react-native-community/async-storage'
+
+
 const width = Dimensions.get('window').width
 const height = Dimensions.get('window').height
 
 const LoginScene = () => {
-  const onPress = () => {
-          alert("clicked");
+  const [token, setToken] = useState('5')
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const saveData = async (STORAGE_KEY, value) => {
+    try {
+      await AsyncStorage.setItem(STORAGE_KEY, value)
+      alert('Data successfully saved')
+    } catch (e) {
+      alert('Failed to save the data to the storage')
     }
-   
+  }
+
+  const readData = async () => {
+    try {
+      const userToken = await AsyncStorage.getItem(STORAGE_KEY)
+
+      if (userToken !== null) {
+        setToken(userToken)
+        console.log('checkcheck')
+
+      }
+    } catch (e) {
+      alert('Failed to fetch the data from storage')
+    }
+  }
+
+  const onPress = () => {
+    fetch('http://10.0.2.2:8000/api/token/', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        username: username,
+        password: password
+      })
+    }).then((response) => response.json())
+      .then((responseJson) => {
+        if (responseJson.detail === undefined) {
+          setToken(responseJson.access)
+          saveData('@token', responseJson.access)
+          saveData('@refreshtoken', responseJson.refresh)
+        }
+        else {
+          alert("Login failed")
+        }
+
+      })
+      ;
+  }
+  const handleUser = (text) => {
+    setUsername(text)
+  }
+
+  const handlePassword = (text) => {
+    setPassword(text)
+  }
+
 
   return (
     <NativeRouter>
-     <ImageBackground source = {backgroundLogin} style = {styles.background}>
-       <View style = {styles.card}>
-         <View style ={{marginBottom : 28}}>
-            <Text style={{fontWeight: "bold",fontSize:27}}>เข้าสู่ระบบ</Text>
+      <ImageBackground source={backgroundLogin} style={styles.background}>
+        <View style={styles.card}>
+          <View style={{ marginBottom: 28 }}>
+            <Text style={{ fontWeight: "bold", fontSize: 27 }}>เข้าสู่ระบบ</Text>
           </View>
-          <View style ={{marginRight :140 ,marginBottom:7}}>
+          <View style={{ marginRight: 140, marginBottom: 7 }}>
             <Text >ชื่อ</Text>
           </View>
-          <InputBox text= "ชื่อ" />
-          <View style ={{marginRight :110,marginBottom:7,marginTop:7}}>
-           <Text>รหัสผ่าน</Text>
-          </View> 
-            <InputBox text= "รหัสผ่าน" />
-            <Text style ={{marginLeft :0 ,marginBottom:7,marginTop:7}} onPress={onPress}  > ลืมรหัสผ่าน?</Text>
-          <ButtonCurve onPress= {onPress} text = "เข้าสู่ระบบ" />
+          <InputBox text={username} onChangeText={handleUser} placeholder="Username" />
+          <View style={{ marginRight: 110, marginBottom: 7, marginTop: 7 }}>
+            <Text>รหัสผ่าน</Text>
+          </View>
+          <InputBox text={password} onChangeText={handlePassword} placeholder="Password" />
+          <Text style={{ marginLeft: 0, marginBottom: 7, marginTop: 7 }} onPress={onPress}  > ลืมรหัสผ่าน?</Text>
+          <ButtonCurve onPress={onPress} text="เข้าสู่ระบบ" />
           <Text onPress={onPress} > สมัครสมาชิกใหม่</Text>
-       </View>
+        </View>
       </ImageBackground>
     </NativeRouter>
   );
-  
+
 }
 
 const styles = StyleSheet.create({
   container: {
-  
+
     //flex: 1,
     //justifyContent: 'center', 
     //alignItems: 'center',
@@ -73,12 +131,12 @@ const styles = StyleSheet.create({
   card: {
     fontFamily: "lucida grande",
     justifyContent: 'center',
-    alignItems : 'center',
-    flexDirection :'column',
-    backgroundColor : 'white',
-    borderRadius : 10,
-    width : width/1.6,
-    height : height/1.8,
+    alignItems: 'center',
+    flexDirection: 'column',
+    backgroundColor: 'white',
+    borderRadius: 10,
+    width: width / 1.6,
+    height: height / 1.8,
   },
   background: {
     justifyContent: 'center',
